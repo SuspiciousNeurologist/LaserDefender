c30d45bd528554e70cd81b6103a013d25ca100d0
AndreW <aldape.andres2@gmail.com>
2019-12-01 00:03:26 -0600

    First Commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{   
+    [Header("Enemy")]
+    [SerializeField] float health = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] int scoreValue = 150;
+    
+    [Header("Enemy Projectile")]
+    [SerializeField] GameObject enemyLaserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
+    
+    [Header("VFX")]
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float durationOfExplosion=1f;
+
+    [Header("SFX")]
+    [SerializeField] AudioClip deathSFX;
+    [SerializeField] [Range(0,1)]float deathSFXVolume = 0.7f;
+    [SerializeField] AudioClip enemyShootSFX;
+    [SerializeField] [Range(0,1)]float enemyShootSFXVolume = 0.7f;
+    
+
+    void Start(){
+        shotCounter = Random.Range(minTimeBetweenShots,maxTimeBetweenShots);
+    }
+
+    void Update(){
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot(){
+        shotCounter -=Time.deltaTime;
+        if(shotCounter<=0f){
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots,maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire(){
+            AudioSource.PlayClipAtPoint(enemyShootSFX,Camera.main.transform.position,enemyShootSFXVolume);
+            GameObject laser = Instantiate(
+                enemyLaserPrefab,
+                transform.position,
+                Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0,-(-projectileSpeed)); 
+    }
+    private void ProcessHit(DamageDealer damageDealer){
+        health -=damageDealer.GetDamage();
+        damageDealer.Hit();
+        if(health <=0){
+            Die();
+        }
+    }
+
+    private void Die(){
+        FindObjectOfType<GameSession>().AddToScore(scoreValue);
+        AudioSource.PlayClipAtPoint(deathSFX,Camera.main.transform.position,deathSFXVolume);
+        Destroy(gameObject);
+        GameObject explosion = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(explosion,durationOfExplosion);
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+        DamageDealer damageDealer =other.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
+    }
+
+
+}
---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    WaveConfig waveConfig;
+    List<Transform> waypoints; 
+    int waypointIndex = 0;
+
+    void Start(){
+        waypoints = waveConfig.getWaypoints();
+        transform.position =waypoints[waypointIndex].transform.position;
+    }
+
+    void Update(){
+        MoveEnemy();
+    }
+
+    public void setWaveConfig(WaveConfig waveConfig)
+    {
+        this.waveConfig = waveConfig;
+    }
+
+    private void MoveEnemy(){
+        if (waypointIndex <= waypoints.Count -1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = waveConfig.getMoveSpeed() * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position,targetPosition,movementThisFrame);
+            if(transform.position==targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
